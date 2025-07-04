2391dbda3006cf906d899730fd31f574d5bda3b1
Simon JAkoby <sikoby@gmx.de>
2025-07-04 12:12:28 +0200

    Minor changes

--- servers/bank-api/routes/user.js
+++ servers/bank-api/routes/user.js
@@ -30,15 +30,4 @@ router.post('/', async (req, res) => {
     }
 });
 
-// Route to get user bank details
-router.get('/:id', async (req, res) => {
-    try {
-        const user = await User.findById(req.params.id);
-        if (!user) return res.status(404).json({ error: 'User not found' });
-        res.json(user);
-    } catch (err) {
-        res.status(400).json({ error: err.message });
-    }
-});
-
 module.exports = router;
